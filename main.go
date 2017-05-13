b794115504968da5b88843d71d75272efc86228d
Yuki Ito <acomagu@gmail.com>
2017-05-13 15:00:53 +0900

    [Add] initial commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"bufio"
+	"os"
+	"os/exec"
+)
+
+const (
+	incomingWebhookURL = ""
+	outgoingWebhookURL = ""
+)
+
+func main() {
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := io.Copy(stdin, r.Body); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		reader := bufio.NewReader(stdout)
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if _, err := w.Write(line); err != nil {
+			fmt.Println(err)
+			return
+		}
+	})
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(http.ListenAndServe(":8000", nil))
+}
